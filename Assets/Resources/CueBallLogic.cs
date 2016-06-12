a72fe273fcebb2b9cefaa3914cdfd2fb1eecb0b4
lukebryant <mb13273@my.bristol.ac.uk>
2016-06-12 20:38:55 +0100

    make velocity physics work without decaying orbit

--- Assets/Resources/CueBallLogic.cs
+++ Assets/Resources/CueBallLogic.cs
@@ -10,7 +10,9 @@ public class CueBallLogic : MonoBehaviour {
 	private Vector2 scaleVector = new Vector2(100f,100f);
 	private Vector2 perpVector = new Vector2(1f,1f);
 	private Vector2 differenceVector;
-	private bool clockwise;
+    private double currentAnchorDistance;
+    private bool clockwise;
+    private bool newAnchor = false;
 	public int id;
     public bool anchored = false;
 
@@ -25,23 +27,50 @@ public class CueBallLogic : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (anchorTransform ==null || !anchored) return;
-        differenceVector = anchorTransform.position - this.transform.position;
-		perpVector = new Vector2 (-differenceVector.y, differenceVector.x);
-		perpVector.Normalize ();
-		perpVector.Scale (scaleVector);
-		if (!clockwise)
-			perpVector *= -1;
-        rigidBody2d.velocity = (Vector3)perpVector;	
 	} 
 
+    void FixedUpdate() { 
+        float time = Time.fixedDeltaTime;
+        if (anchorTransform == null || !anchored) return;
+        if (newAnchor)
+        {
+            differenceVector = anchorTransform.position - this.transform.position;
+            perpVector = new Vector2(-differenceVector.y, differenceVector.x);
+            perpVector.Normalize();
+            perpVector.Scale(scaleVector);
+            if (!clockwise)
+                perpVector *= -1;
+            rigidBody2d.velocity = (Vector3)perpVector;
+            newAnchor = false;
+            currentAnchorDistance = Vector3.Magnitude(differenceVector);
+        }
+        else
+        {
+            double circumference = currentAnchorDistance * 2 * Mathf.PI;
+            double orbitTime = circumference / Vector3.Magnitude(rigidBody2d.velocity);
+            double angleRotation = (clockwise ? -1 : 1) * 360 * (Time.fixedDeltaTime / orbitTime);
+            rigidBody2d.velocity = Quaternion.Euler(0, 0, (float)angleRotation) * rigidBody2d.velocity;
+        }
+    }
+
 	public void setCurrentAnchor(Transform newAnchorTransform){
-		anchorTransform = newAnchorTransform;
+        if (anchorTransform != null)
+        {
+            differenceVector = anchorTransform.position - this.transform.position;
+            perpVector = new Vector2(-differenceVector.y, differenceVector.x);
+            perpVector.Normalize();
+            perpVector.Scale(scaleVector);
+            if (!clockwise)
+                perpVector *= -1;
+        }
+        anchorTransform = newAnchorTransform;
 		differenceVector = newAnchorTransform.position - this.transform.position;
 		float angle = Vector2.Angle (perpVector, differenceVector);
 		Vector3 cross = Vector3.Cross (perpVector, differenceVector);
 		if (cross.z > 0)
 			angle = 0 - angle;
-		clockwise = true ? angle > 0 : false;
+		clockwise = angle > 0;
+        newAnchor = true;
+        print(clockwise ? "clockwise" : "anticlockwise");
 	}
 }
